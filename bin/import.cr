540a67ca72cf9d89e79ea66faf55a70af7bcee5e
Brian J. Cardiff <bcardiff@manas.com.ar>
2015-03-19 02:25:07 -0300

    split import tool

--- bin/import.cr
+++ bin/import.cr
@@ -1,59 +1,4 @@
-require "ecr/macros"
-require "../src/crocoa"
-
-include Crocoa
-
-class ClassView
-  getter :nsclass
-  def initialize(@nsclass, @docs)
-  end
-  ecr_file "#{__DIR__}/import_class.ecr"
-
-  def crystal_method_for(method : NSMethod)
-    method.name.underscore.gsub(/([A-Z]+)([A-Z])([a-z])/) do |str, match|
-      "#{match[1].downcase}_#{match[2].downcase}#{match[3]}"
-    end
-  end
-
-  def source_location(nsclass)
-    "./#{crystal_class_name(nsclass).downcase}"
-  end
-
-  def crystal_class_name(nsclass : NSClass)
-    crystal_class_name(nsclass.name)
-  end
-
-  def crystal_class_name(nsclass_name : String)
-    nsclass_name.gsub(/^_+/) { "" }
-  end
-
-  def return_type_for(method : NSMethod)
-    method.return_type
-  end
-
-  def strong_return_type_for(method : NSMethod)
-    @docs.return_type(method.name)
-  end
-
-  def send_message_for(method : NSMethod)
-    if method.return_type == "double"
-      :send_msg_fpret
-    elsif method.return_type == "float"
-      :send_msg_float32
-    else
-      :send_msg
-    end
-  end
-end
-
-class Docs
-  def initialize(@d = {} of String => String)
-  end
-
-  def return_type(method_name)
-    @d[method_name]?
-  end
-end
+require "../src/tools/import"
 
 def generate_class(class_name, docs, path = nil)
   content = ClassView.new(NSClass.new(class_name), docs).to_s
@@ -70,4 +15,3 @@ end
 generate_class "NSString", Docs.new({"uppercaseString" => "NSString"}), "../src/foundation/generated/nsstring.cr"
 # generate_class "NSMutableString", "../src/foundation/generated/nsmutablestring.cr"
 # generate_class "__NSCFString", "../src/foundation/generated/__nscfstring.cr"
-
